7329012b97b47094dad468115b90b41cd43d8595
Mauro <mauro.valenzuelatobar@gmail.com>
2023-09-27 22:32:16 -0300

    Avancé un poco, tengo pensado migrar de sistema linux

--- script.js
+++ script.js
@@ -1,21 +1,36 @@
 //Parametro size recibirá el argumento del tamaño del lienzo (El usuario determinará el tamaño)
 function boardSize(size){
 
-    let board = document.querySelector('.board')
+    let board = document.querySelector('.board');
+    let square = board.querySelectorAll('div')
+
+    //square.forEach((div) => div.remove()); Eliminar los pintados?
+    
+
     board.style.gridTemplateColumns = `repeat(${size}, 1fr)`//Tamaño por defecto 16x16
     board.style.gridTemplateRows = `repeat(${size}, 1fr)`
 
-    for(let i = 0; i<256; i++){
+    //El lienzo se ajustará al nuevo tamaño, y no habrá error de distribución.
+    let divAmount = size * size
+    for(let i = 0; i<divAmount; i++){
         let square = document.createElement('div');
+        square.addEventListener('mouseover', ()=>{square.style.backgroundColor = "green"})
         square.style.backgroundColor = "yellow"
         board.appendChild(square);//Se puede usar tambien insertAdjacentElement
     };
 
 };
 
-boardSize(32);
+boardSize(16);
 
 //Función que recibirá el tamaño colocado por el usuario.
 function changeSize(input){
-    boardSize(input);
+    if (input >= 3 && input <= 256){//Tamaños aceptados
+        boardSize(input);
+    } else {
+        console.log("NO aceptada la cantidad")
+    }
+
+
+    
 };
