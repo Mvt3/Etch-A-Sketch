3420057ee8e83e297c1bc140c4ae2c257bdde0f5
Aqua <mauro.valenzuelatobar@gmail.com>
2023-09-29 21:21:39 -0300

    Ya está el 90% de la logica lista

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
+boardSize(16);
+let color="white";
+
 //Parametro size recibirá el argumento del tamaño del lienzo (El usuario determinará el tamaño)
 function boardSize(size){
 
     let board = document.querySelector('.board');
     let square = board.querySelectorAll('div')
 
-    //square.forEach((div) => div.remove()); Eliminar los pintados?
+    square.forEach((div) => div.remove()); //Eliminar todo, para cuando se cambie el tamaño, este limpio
     
 
     board.style.gridTemplateColumns = `repeat(${size}, 1fr)`//Tamaño por defecto 16x16
@@ -14,18 +17,16 @@ function boardSize(size){
     let divAmount = size * size
     for(let i = 0; i<divAmount; i++){
         let square = document.createElement('div');
-        square.addEventListener('mouseover', ()=>{square.style.backgroundColor = "green"})
-        square.style.backgroundColor = "yellow"
+        square.addEventListener('mouseover', divColor)
+        square.style.backgroundColor = "white"
         board.appendChild(square);//Se puede usar tambien insertAdjacentElement
     };
 
 };
 
-boardSize(16);
-
 //Función que recibirá el tamaño colocado por el usuario.
 function changeSize(input){
-    if (input >= 3 && input <= 256){//Tamaños aceptados
+    if (input >= 3 && input <= 150){//Tamaños aceptados
         boardSize(input);
     } else {
         console.log("NO aceptada la cantidad")
@@ -34,3 +35,32 @@ function changeSize(input){
 
     
 };
+
+//Establece el color
+function divColor(){
+
+    if(color === "rainbow"){
+        this.style.backgroundColor = `hsl(${Math.random()*360}, 100%, 50%)`;
+    }else{
+
+        this.style.backgroundColor = color;//This se refiere al elemento HTML que activa la funct
+    };
+
+   
+};
+
+
+//Cambia el color
+function colorChange(choice){
+    color = choice;
+    
+    
+};
+
+//Borra todo lo pintado en el tablero
+function resetBoard(){
+    let board = document.querySelector('.board');
+    let square = board.querySelectorAll('div')
+    square.forEach((div) => div.style.backgroundColor="white");
+}
+
